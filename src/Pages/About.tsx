207ebd053a45beee314f86829c40b1f53cf01b0f
Hunter Carver <hunter@carvertechllc.com>
2022-11-12 08:57:44 -0600

    Added the typewritter title

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,8 +1,18 @@
-import { Badge, useMantineColorScheme, useMantineTheme } from '@mantine/core';
+import { Badge, createStyles, Title, useMantineColorScheme, useMantineTheme } from '@mantine/core';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import Typewriter from 'typewriter-effect';
+
+const useStyles = createStyles((theme) => ({
+  title: {
+    [theme.fn.smallerThan('md')]: {
+      fontSize: 18,
+    },
+  }
+}));
 
 function About () {
   const { colorScheme } = useMantineColorScheme();
+  const { classes, cx } = useStyles();
   const theme = useMantineTheme();
   
   var winWidth = window.innerWidth;
@@ -14,6 +24,7 @@ function About () {
   "MUI", "MATLAB", "Version Control", "Java", "JQuery", "AJAX", "REST", "Entity Framework", "Entity Framework Core", "Stored Procedures",
   "Package Managers", "NPM", "Yarn", "NuGet", "Datatables", "CLI", "NodeJS", "Mantine", "TypeScript", "Postman", "APIS", "Web APIs"];
   var badges: JSX.Element[] = [];
+  let text = 'Full-Stack Software Engineer';
 
   skills.forEach((skill) => {
     let randomTop = getRandomNumber(0, winHeight);
@@ -41,7 +52,15 @@ function About () {
           offset={0}
           speed={2.5}
           style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <p>Scroll down</p>
+            <Title className={classes.title}>
+              <Typewriter
+                options={{
+                  strings: [text],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </Title>
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={2} style={{ backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background }} />
@@ -70,10 +89,14 @@ export default About;
 function getRandomNumber(min: number, max: number) {
     
   var pos = Math.floor(Math.random() * (max - min + 1)) + min;
+  //To make sure the skills are not too close to the edge of the screen
   if(pos > max - 100)
     getRandomNumber(min, max);
   else if(pos < min + 100)
     getRandomNumber(min, max);
+  //To make sure the skills are not too close to the the typewriter text
+  else if(pos < (max/2) + 50 && pos > (max/2) - 50)
+      getRandomNumber(min, max);
   else
     return pos;
 
